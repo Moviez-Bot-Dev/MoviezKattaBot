2ed60ac055fe6160fcb87f58994c542129bf910a
Moviez Bot Dev <143684485+Moviez-Bot-Dev@users.noreply.github.com>
2024-01-26 13:10:09 +0530

    Add files via upload

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -35,6 +35,7 @@ class Database:
             title = title,
             chat_status=dict(
                 is_disabled=False,
+                is_mania_verified=False,
                 reason="",
             ),
         )
@@ -86,10 +87,18 @@ class Database:
     async def get_banned(self):
         users = self.col.find({'ban_status.is_banned': True})
         chats = self.grp.find({'chat_status.is_disabled': True})
+        is_verified = self.grp.find({'chat_status.is_mania_verified': True})
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]
-        return b_users, b_chats
-
+        mz_verified = [chat['id'] async for chat in is_verified]
+        return b_users, b_chats, mz_verified
+  
+    async def verify_mania_chat(self, chat):
+        chat_status=dict(
+            is_mania_verified=True,
+            )
+        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
+    
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
         await self.grp.insert_one(chat)
